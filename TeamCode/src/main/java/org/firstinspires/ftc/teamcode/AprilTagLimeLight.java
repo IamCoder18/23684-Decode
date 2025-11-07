60b734b17db9d0e7fa434496ebb7cf1e6a93f1ea
jaydenwithawhy <jaydenuvbiama@gmail.com>
2025-11-06 18:36:30 -0700

    Intake actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimeLight.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+
+@TeleOp
+public class AprilTagLimeLight extends OpMode {
+
+    private Limelight3A limelight;
+
+    private IMU imu;
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot orientation = new RevHubOrientationOnRobot(
+        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
+        );
+        imu.initialize(new IMU.Parameters(orientation));
+    }
+
+    public void start() {
+        limelight.start();
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(angles.getYaw());
+        LLResult llResult = limelight.getLatestResult();
+        if (llResult != null && llResult.isValid()){
+            Pose3D botPose = llResult.getBotpose_MT2();
+            telemetry.addData("Tx", llResult.getTx());
+            telemetry.addData("Ty", llResult.getTy());
+            telemetry.addData("Ta", llResult.getTa());
+            telemetry.addData("BotPose", botPose.toString());
+            telemetry.addData("Orientation", botPose.getOrientation().toString());
+
+        }
+
+    }
+}
